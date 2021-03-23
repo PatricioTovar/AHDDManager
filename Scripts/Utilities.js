22d1b8301ac9f97c9314c05472a93b8cd38e4b97
Patricio Tovar <patricio.tovar97@hotmail.com>
2021-03-23 13:01:25 -0500

    Fix Add transaction Item

--- Scripts/Utilities.js
+++ Scripts/Utilities.js
@@ -41,18 +41,15 @@ function numericValidate(evt) {
     }
 }
 
-var formatterCurrency = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-
-    // These options are needed to round to whole numbers if that's what you want.
-    //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-    //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
-});
-
 //https://jqueryui.com/autocomplete/#combobox
-$(function () {
+(function ($) {
     $.widget("custom.combobox", {
+
+        options: {
+            value: 0,
+            text: '',
+        }, 
+
         _create: function () {
             this.wrapper = $("<div>")
                 .attr("id", this.element.attr("id") + "-wrapper")
@@ -173,10 +170,11 @@ $(function () {
             this.element.show();
         },
 
-        clear: function () {
+        clear: function (x) {
             this.input.val("")
             this.element.val("").change();
         }
     });
 
-});
+
+})(jQuery);
